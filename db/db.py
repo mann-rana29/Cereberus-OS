00d2e7afede1b3c4fc49bc1bcc7516d9e8efe197
the25thjin <mannr075@gmail.com>
2026-07-10 00:14:51 +0530

    configured sqlite db

---
+++ db/db.py
@@ -0,0 +1,40 @@
+import sqlite3
+import os
+
+DB_PATH=os.path.join(os.path.dirname(__file__), "data" , "active_plant.db")
+
+def get_connection():
+    os.makedirs(os.path.dirname(DB_PATH),exist_ok=True)
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+
+    return conn
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS permits(
+            permit_id TEXT PRIMARY KEY,
+            zone_id TEXT NOT NULL,
+            work_type TEXT NOT NULL,
+            assigned_team TEXT NOT NULL,
+            status TEXT DEFAULT 'ACTIVE',
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+    )
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS sensor_logs(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            zone_id TEXT NOT NULL,
+            gas_type TEXT NOT NULL,
+            gas_ppm REAL NOT NULL,
+            triggered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+    )
---
+++ main.py
@@ -0,0 +1,5 @@
+from db.db import init_db
+
+init_db()
+
+print('DB ready!')
